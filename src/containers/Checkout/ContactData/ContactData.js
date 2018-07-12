e44f004d1a08185fc8f329d9b470ab86ce37be8c
GCoder10 <https://github.com/GCoder10/Web-api.git>
2018-07-12 17:32:50 +0200

    React forms
    React forms

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,7 +3,7 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
-
+import Input from '../../../components/UI/Input/Input';
 
 
 /*
@@ -14,13 +14,103 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
     nastepuje przekierowanie do innej strony
 */
 
+/*
+    Kazdy input bedzie oddzielnym komponentem
+    UI/Input
+    Input komponent tak dostosowany, zeby wyswietlal 
+    odpowiedni element formularza w zaleznosci od tego
+    jakie parametry tam zostana wyslane poprzez obiekt
+    props
+*/
+
+/*
+    Wygenerowanie odpowiednich inputow o odpowiednich
+    typach poprzez uzycie petli zawierajacej elementy
+    obiektu orderForm:
+    1. Trzeba przerobic obiekt orderForm w taki sposob, 
+    zeby uzyskac tablice.
+    2. Kazdy key to jest caly oddzielny obiekt, np.
+    street, zipCode, name
+    natomiast config jest to prawa strona takiego obiektu,
+    np. elementType, value:
+
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+        formElementsArray.push({
+            id: key,
+            config: this.state.orderForm[key]
+        });
+    }
+
+    3. Tak stworzona tablice formElementsArray mozna teraz 
+    wykorzystac w petli do tworzenia poszczegolnych inputow:
+
+        { 
+        formElementsArray.map(formElement => (
+            <Input elementType = { formElement.config.elementType } 
+                   elementConfig = { formElement.config.elementConfig } 
+                   value = { formElement.config.value } 
+                   key = { formElement.id } />
+        ))
+        }
+
+*/
+
+
+
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [ 
+                        { value: 'fastest', displayValue: 'Fastest' },
+                        { value: 'cheapest', displayValue: 'Cheapest' }
+                    ]
+                },
+                value: ''
+            },
         },
         loading: false
     }
@@ -31,17 +121,7 @@ class ContactData extends Component {
         this.setState({ loading: true });
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price,
-            customer: {
-                name: 'Max',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '41351',
-                    country: 'Poland'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+            price: this.props.price
         }    
         axios.post('/orders.json', order)
             .then(response => {
@@ -56,28 +136,24 @@ class ContactData extends Component {
     }
 
     render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+        formElementsArray.push({
+            id: key,
+            config: this.state.orderForm[key]
+        });
+    }
+
         let form = (
                 <form>
-                    <input className = { classes.Input } 
-                           type = "text" 
-                           name = "name" 
-                           placeholder = "Your Name" />
-
-                    <input className = { classes.Input } 
-                           type = "email" 
-                           name = "email" 
-                           placeholder = "Your Mail" />
-
-                    <input className = { classes.Input } 
-                           type = "text" 
-                           name = "street" 
-                           placeholder = "Street" />
-
-                    <input className = { classes.Input } 
-                           type = "text" 
-                           name = "postal" 
-                           placeholder = "Postal Code" />
-
+                    { 
+                    formElementsArray.map(formElement => (
+                        <Input elementType = { formElement.config.elementType } 
+                               elementConfig = { formElement.config.elementConfig } 
+                               value = { formElement.config.value } 
+                               key = { formElement.id } />
+                    ))
+                    }
                     <Button btnType = "Success" 
                             clicked = { this.orderHandler } >
                             ORDER
