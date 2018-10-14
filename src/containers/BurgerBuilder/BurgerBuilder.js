ad7c4201e6cbe21e1bc1f27cb63166b850df72d9
GCoder10 <https://github.com/GCoder10/Web-api.git>
2018-10-14 20:21:24 +0200

    commit
    commit

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,18 +4,15 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-
+import axios from '../../axios-orders';
 import { connect } from 'react-redux';
-import * as actionTypes from '../../store/actions';
+import * as burgerBuilderActions from '../../store/actions/index';
 
 class BurgerBuilder extends Component {
     state = {
-        purchasing: false,
-        loading: false,
-        error: false
+        purchasing: false
     }
 
 /*
@@ -45,16 +42,6 @@ class BurgerBuilder extends Component {
 */
 
 
-componentDidMount() {
-//    axios.get('https://react-my-burger-6e0f8.firebaseio.com/ingredients.json')
-//         .then(response => {
-//                this.setState({ ingredients: response.data })})
-//         .catch(error => {
-//                this.setState({ error: true })
-//         });
-}
-
-    
 updatePurchaseState(ingredients) {
     // Sprawdzenie czy mozna juz zamowic burger
     // 1. Korzystanie z najnowszej tablicy ingredients zamiast
@@ -135,9 +122,6 @@ purchaseContinueHandler = () => {
                                          purchaseContinued = { this.purchaseContinueHandler } 
                                          price = { this.props.price } />;       
         }
-        if (this.state.loading) {
-            orderSummary = <Spinner />;
-        }
 
         return (
             <Aux>
@@ -159,8 +143,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIngredientAdded: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
-        onIngredientRemoved: (ingName) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName})
+        onIngredientAdded: (ingName) => dispatch( burgerBuilderActions.addIngredient(ingName) ),
+        onIngredientRemoved: (ingName) => dispatch( burgerBuilderActions.removeIngredient(ingName) )
     }
 }
 
