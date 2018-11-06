334198b3d30ae8d44e9b401d6a92253eab8aa771
GCoder10 <https://github.com/GCoder10/Web-api.git>
2018-11-06 23:36:56 +0100

    commit
    commit

---
+++ src/store/actions/auth.js
@@ -0,0 +1,42 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = (authData) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        authData: authData
+    };
+};
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+
+export const auth = (email, password) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyA5nnrurfJqBsuDUeKfh4y9O_GA0eIfPu4', authData)
+             .then(response => {
+                    console.log(response);
+                    dispatch(authSuccess(response.data));
+             })
+             .catch(err => {
+                    console.log(err);
+                    dispatch(authFail(err));
+             });
+    };
+};
